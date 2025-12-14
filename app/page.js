e45f8015988d7bcf7656a9895468e577e5c635c5
ArthurJuwer <arthur.juwer99@gmail.com>
2025-12-14 18:45:57 -0300

    Addition Header component and new font style DM SANS

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import Footer from "@/components/Footer";
+import Header from "@/components/Header";
 import Team from "@/components/Team";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
+      <Header />
       <Team />
       <Footer />
     </div>
---
+++ components/Header.jsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import TextLogo from "../public/images/logos/TextLogo.png";
+import BrasilFlag from "../public/images/languages/Brasil.png";
+
+export default function Header() {
+  return (
+    <div className="flex items-center justify-between p-10 bg-[#0F224A] text-white font-bold">
+        <nav className="flex items-center gap-x-6">
+        <Image src={TextLogo} width={170} height={60} alt="" />
+            <ul className="flex items-center gap-x-10">
+                <li>
+                    <button className="bg-[#1A3879] cursor-pointer  px-10 -mr-4 py-3 rounded-full">JOGAR</button>
+                </li>
+                <li>APRESENTAÇÃO</li>
+                <li>FEIRAS</li>
+                <li>TRABALHO</li>
+                <li>COMUNIDADE</li>
+                <li>EQUIPE</li>
+            </ul>
+        </nav>
+        <div className="flex items-center justify-center gap-x-4">
+            <button className="bg-[#F9A318] cursor-pointer px-12 py-3 rounded-full" >DISPONÍVEL AGORA</button>
+            <button>
+                <Image src={BrasilFlag} width={35} height={25} alt="" />
+            </button>
+        </div>
+        
+    </div>
+  )
+}
